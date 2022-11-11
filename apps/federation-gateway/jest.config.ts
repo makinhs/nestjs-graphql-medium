74e5aa12fa51efdf4cc9c58bcf5f8677da271f1c
Marcos Silva <tio.makin@gmail.com>
2022-11-11 11:30:05 +0100

    Initial commit

---
+++ apps/federation-gateway/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'federation-gateway',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/federation-gateway',
+};
